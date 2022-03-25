a65e9d8045194ba3b111a69237466d3f9893deb0
lukas <lucass.lv.lv@gmail.com>
2022-03-24 22:27:05 -0300

    menu lateral estilizado

--- src/Components/LateralMenu/index.tsx
+++ src/Components/LateralMenu/index.tsx
@@ -1,16 +1,91 @@
 import React from 'react';
 
 // Components
-import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
 // styles
-import { Drawer } from './styles';
+import { Drawer, AddCard } from './styles';
+
+// Assets
+import logoProject from '../../Assets/images/logoProject.png';
+import { ReactComponent as MenuIcon } from '../../Assets/icons/menuIcon.svg';
+import { ReactComponent as GridIcon } from '../../Assets/icons/grid.svg';
+import { ReactComponent as BullseyeArrow } from '../../Assets/icons/bullseyeArrow.svg';
+import { ReactComponent as Cadastrar } from '../../Assets/icons/Cadastrar.svg';
+import { ReactComponent as CalendarClock } from '../../Assets/icons/calendarClock.svg';
+import { ReactComponent as Configurar } from '../../Assets/icons/Configurar.svg';
+import { ReactComponent as FileChart } from '../../Assets/icons/fileChart.svg';
+import { ReactComponent as Notificacao } from '../../Assets/icons/notificacao.svg';
 
 function LateralMenu() {
+  const [addCartHover, setAddCartHover] = React.useState(false);
+
   return (
     <div>
       <Drawer variant='permanent' anchor='left'>
-        <Box sx={{ overflowX: 'hidden' }}>a</Box>
+        <img className='logo' src={logoProject} alt='logoProject' />
+
+        <List>
+          <ListItem button className='item'>
+            <ListItemIcon>
+              <MenuIcon className='icon' />
+            </ListItemIcon>
+          </ListItem>
+
+          <ListItem button className='item'>
+            <ListItemIcon>
+              <GridIcon className='icon' />
+            </ListItemIcon>
+          </ListItem>
+
+          <ListItem button className='item'>
+            <ListItemIcon>
+              <Notificacao className='icon' />
+            </ListItemIcon>
+          </ListItem>
+
+          <ListItem button className='item'>
+            <ListItemIcon>
+              <CalendarClock className='icon' />
+            </ListItemIcon>
+          </ListItem>
+
+          <ListItem button className='item'>
+            <ListItemIcon>
+              <BullseyeArrow className='icon' />
+            </ListItemIcon>
+          </ListItem>
+
+          <ListItem button className='item'>
+            <ListItemIcon>
+              <FileChart className='icon' />
+            </ListItemIcon>
+          </ListItem>
+
+          <ListItem button className='item'>
+            <ListItemIcon>
+              <Cadastrar className='icon' />
+            </ListItemIcon>
+          </ListItem>
+
+          <ListItem button className='item'>
+            <ListItemIcon>
+              <Configurar className='icon' />
+            </ListItemIcon>
+          </ListItem>
+        </List>
+
+        <div>
+          <AddCard
+            className='item'
+            onMouseOver={() => setAddCartHover(true)}
+            onMouseOut={() => setAddCartHover(false)}
+          >
+            {addCartHover ? '+ CRIAR CARD' : '+'}
+          </AddCard>
+        </div>
       </Drawer>
     </div>
   );
--- src/Components/LateralMenu/styles.ts
+++ src/Components/LateralMenu/styles.ts
@@ -9,8 +9,57 @@ export const Drawer = styled(MuiDrawer, {
   width: drawerWidth,
   flexShrink: 0,
   '& .MuiDrawer-paper': {
+    boxShadow: '20px 35px 50px #0000001A',
+    overflow: 'hidden',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     width: drawerWidth,
+    padding: '5px 0px',
     boxSizing: 'border-box',
     background: theme.palette.primary.light,
   },
+
+  '.logo': {
+    width: '59px',
+    height: '59px',
+    borderRadius: '50px',
+    boxShadow: '0px 3px 6px #00000029',
+  },
+
+  '.icon': {
+    width: '20px',
+    height: '20px',
+    margin: '0 auto',
+    fill: theme.palette.primary.dark,
+    color: theme.palette.primary.dark,
+  },
+
+  '.item': {
+    margin: '10px 0px',
+  },
 }));
+
+export const AddCard = styled('div')`
+  width: 30px;
+  height: 30px;
+  font-size: 18px;
+  font-weight: 700;
+  border-radius: 50px;
+  background: #d60297;
+  color: #fff;
+  box-shadow: 0px 3px 6px #00000029;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover {
+    width: 157px;
+    height: 36px;
+    border-radius: 10px;
+    margin-left: -15px;
+    position: fixed;
+  }
+`;
