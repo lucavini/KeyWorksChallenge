d6f9069829a66fea4964d6594351c8b2b1c86f14
lukas <lucass.lv.lv@gmail.com>
2022-03-27 13:19:18 -0300

    card de projeto estilizado

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,13 +1,113 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
-import { CardProject, Type, Header } from './styles';
+// Components
+import { styled } from '@mui/material/styles';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import { ReactComponent as Calendar } from '../../Assets/icons/Calendar.svg';
+import { ReactComponent as Clock } from '../../Assets/icons/Clock.svg';
+
+// Styles
+import {
+  CardProject,
+  Type,
+  Header,
+  Title,
+  Infos,
+  Description,
+  Following,
+  Schedule,
+  Status,
+  Team,
+  TeamCircle,
+} from './styles';
+
+const CustomWidthTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    maxWidth: 178,
+  },
+});
+
+const description = `Usar a branch master, fazer pull, após isso rodar o comando "edge
+migration".`;
 
 function Card() {
   return (
     <CardProject>
       <Header>
         <Type>DESENVOLVIMENTO</Type>
+        <div className='code'>
+          <p className='label'>Código:</p>
+          <p className='code-number'>#12345</p>
+        </div>
       </Header>
+
+      <Title>criar migration</Title>
+
+      <Infos>
+        <div className='project'>
+          <p className='label'>Projeto:</p>
+          <p className='project-name'>Company</p>
+        </div>
+
+        <div className='schedule'>
+          <p className='label'>Prevista:</p>
+          <p className='date-label'>
+            <Calendar className='calendar' />
+            <span className='date'>30/12/2021</span>
+          </p>
+        </div>
+      </Infos>
+
+      <Description>
+        <p className='label'>Descrição:</p>
+        <CustomWidthTooltip title={description} placement='right-end'>
+          <p>{description}</p>
+        </CustomWidthTooltip>
+      </Description>
+
+      <Following>
+        <span>Acompanhamento</span>
+      </Following>
+
+      <Schedule>
+        <div className='project'>
+          <p className='label'>Previsto:</p>
+          <p className='time'>
+            <Clock className='icon' /> 00:30
+          </p>
+        </div>
+        <Status>Em dia</Status>
+      </Schedule>
+
+      <Team>
+        <div className='project'>
+          <p className='label'>Saldo:</p>
+          <p className='time'>
+            <Clock className='icon' /> <span>+00:30</span>
+          </p>
+        </div>
+
+        <div className='teams'>
+          <p className='label'>Equipe:</p>
+          <div className='teams-circles'>
+            <TeamCircle>
+              <span>AS</span>
+            </TeamCircle>
+
+            <TeamCircle>
+              <span>PH</span>
+            </TeamCircle>
+
+            <TeamCircle>
+              <span>WO</span>
+            </TeamCircle>
+          </div>
+        </div>
+      </Team>
     </CardProject>
   );
 }
--- src/Components/Card/styles.ts
+++ src/Components/Card/styles.ts
@@ -1,17 +1,33 @@
 import styled from 'styled-components';
 
 export const CardProject = styled.div`
+  position: relative;
   width: 320px;
   height: 248px;
   margin-top: 16px;
-  padding: 13px 20px;
+  padding: 13px 24px;
   background: #ffffff 0% 0% no-repeat padding-box;
   box-shadow: 0px 3px 6px #00000029;
   border-radius: 5px;
+
+  .label {
+    color: #00000080;
+    font-size: 10px;
+    text-transform: capitalize;
+  }
 `;
 
 export const Header = styled.div`
-    
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .code {
+    .code-number {
+      color: #000000e6;
+      font-size: 12px;
+    }
+  }
 `;
 
 export const Type = styled.span`
@@ -21,6 +37,190 @@ export const Type = styled.span`
   border-radius: 3px;
   letter-spacing: 0px;
   color: #00000080;
-  text-transform: uppercase;
   border: 1px solid #00000080;
+  text-transform: uppercase;
+`;
+
+export const Title = styled.h2`
+  margin-top: 10px;
+  font-size: 14px;
+  font-weight: 600;
+  color: #000000e6;
+  text-transform: uppercase;
+`;
+
+export const Infos = styled.div`
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .schedule {
+    .label {
+      text-align: right;
+    }
+  }
+
+  .project-name {
+    color: #000000e6;
+    font-size: 14px;
+    font-weight: 600;
+  }
+
+  .date-label {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .calendar {
+    width: 20px;
+    height: 20px;
+    margin-bottom: 2px;
+  }
+
+  .date {
+    font-size: 14px;
+    color: #000000e5;
+  }
+`;
+
+export const Description = styled.div`
+  margin-top: 10px;
+
+  p {
+    font-size: 12px;
+    color: #000000b3;
+    white-space: nowrap;
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const Following = styled.p`
+  position: absolute;
+  margin-top: 15px;
+  left: 0;
+  color: #00000080;
+  font-size: 10px;
+  text-transform: capitalize;
+  width: 100%;
+  text-align: center;
+  border-bottom: 1px solid #0000004d;
+  line-height: 0.1em;
+
+  span {
+    position: absolute;
+    left: 24px;
+    background: #fff;
+    padding: 0 2px;
+  }
+`;
+
+export const Schedule = styled.div`
+  margin-top: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .project {
+    text-align: left;
+
+    .label {
+      margin-left: 20px;
+    }
+  }
+
+  .time {
+    font-size: 11px;
+    display: flex;
+    align-items: center;
+    .icon {
+      width: 11px;
+      height: 11px;
+      margin-right: 8px;
+      fill: ${({ theme }) => theme.palette.secondary.dark};
+    }
+  }
+`;
+
+export const Status = styled.div`
+  width: 80px;
+  height: 24px;
+  margin-top: 3px;
+  background: ${({ theme }) => theme.palette.primary.main};
+  box-shadow: 0px 3px 6px #00000029;
+  border-radius: 3px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 11px;
+  font-weight: 700;
+`;
+
+export const Team = styled.div`
+  margin-top: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .project {
+    text-align: left;
+
+    .label {
+      margin-left: 20px;
+    }
+  }
+
+  .time {
+    font-size: 11px;
+    display: flex;
+    align-items: center;
+
+    span {
+      border: 1px solid ${({ theme }) => theme.palette.primary.main};
+      border-radius: 3px;
+      color: ${({ theme }) => theme.palette.primary.main};
+      font-weight: 600;
+      font-size: 10px;
+      padding: 1px 2px;
+    }
+
+    .icon {
+      width: 11px;
+      height: 11px;
+      margin-right: 8px;
+      fill: ${({ theme }) => theme.palette.secondary.dark};
+    }
+  }
+
+  .teams {
+    .teams-circles {
+      display: flex;
+      justify-content: right;
+      align-items: center;
+    }
+
+    .label {
+      text-align: right;
+    }
+  }
+`;
+
+export const TeamCircle = styled.div`
+  width: 20px;
+  height: 20px;
+  margin-left: 5px;
+  border-radius: 50px;
+  background: ${({ theme }) => theme.palette.secondary.main};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  span {
+    font-size: 9px;
+    color: #fff;
+  }
 `;
