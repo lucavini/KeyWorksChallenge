9d3adee8079b121024363c95dbe7f73612c4348f
lukas <lucass.lv.lv@gmail.com>
2022-03-28 09:44:41 -0300

    card finalizado

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-// styles
+
+// Styles
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
 import { ThemeProvider as SCThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/Global';
+
+// Themes
 import MUITheme from '../styles/Default/Mui';
 import SCTheme from '../styles/Default/Styled';
+
+// Components
 import Home from '../Components/Home';
 import { ProjectProvider } from '../Context/ProjectContext';
 
