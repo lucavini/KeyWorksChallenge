3f79785466a91475547bfd99809dea8ea3a9492a
Lucas Santos <lucas.santos@mblabs.com.br>
2022-12-04 17:43:54 -0300

    feat: baseurl

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://kanban-back2.eba-fr8ukxqm.us-east-2.elasticbeanstalk.com',
 });
 
 export default api;
